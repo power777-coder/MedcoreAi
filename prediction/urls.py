d417c7c58da23784acd049c90fd84b6bc3c9247e
Kresha Shah <shahkresha953@gmail.com>
2026-03-18 15:50:27 +0530

    updated urls.py

--- prediction/urls.py
+++ prediction/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-
-from .views import chat_predict, get_symptoms, predict_disease, prediction_history
+from . import views
 
 urlpatterns = [
-    path("predict/", predict_disease),
-    path("chatbot/", chat_predict),
-    path("history/", prediction_history),
-    path("symptoms/", get_symptoms),
+    path("predict/", views.predict_disease),
+    path("history/", views.prediction_history),
+    path("symptoms/", views.get_symptoms),
+    path("chat/", views.chat_predict),
 ]
